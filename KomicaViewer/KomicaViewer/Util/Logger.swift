6244500e5670a7192a9f40db4a1ea8bcc9149f14
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-08-10 18:34:17 +1000

    Added the Logger class from one of my previous project.

---
+++ KomicaViewer/KomicaViewer/Util/Logger.swift
@@ -0,0 +1,23 @@
+//
+//  Logger.swift
+//  Exellency
+//
+//  Created by Craig Zheng on 9/07/2016.
+//  Copyright © 2016 cz. All rights reserved.
+//
+
+import Foundation
+
+func DLog(message: String, filename: String = #file, function: String = #function, line: Int = #line) {
+    #if DEBUG
+        NSLog("%@","[\((filename as NSString).lastPathComponent):\(line)] \(function) - \(message)")
+    #endif
+}
+
+func DLog(error: ErrorType?, filename: String = #file, function: String = #function, line: Int = #line) {
+    if let error = error {
+        DLog("\(error)", filename: filename, function: function, line: line)
+    } else {
+        DLog("Unknown Error", filename: filename, function: function, line: line)
+    }
+}
