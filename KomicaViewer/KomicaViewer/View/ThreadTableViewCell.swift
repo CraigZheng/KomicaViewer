154faca5f3aa403e1e3b605dd226c108e5670f4f
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-08-10 19:12:47 +1000

    Added THreadTableViewCell class to the project, this view will be responsible for displaying the content of a thread.

---
+++ KomicaViewer/KomicaViewer/View/ThreadTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ThreadTableViewCell.swift
+//  KomicaViewer
+//
+//  Created by Craig Zheng on 10/08/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+class ThreadTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
