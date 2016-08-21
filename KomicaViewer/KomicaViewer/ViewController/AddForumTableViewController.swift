7f52c85209d76e9fd59f34f2415103099c144848
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-08-21 13:54:54 +1000

    Added AddForumTableViewController and ForumTextInputViewController.

---
+++ KomicaViewer/KomicaViewer/ViewController/AddForumTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddForumTableViewController.swift
+//  KomicaViewer
+//
+//  Created by Craig Zheng on 21/08/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+class AddForumTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+
+}
---
+++ KomicaViewer/KomicaViewer/ViewController/ForumTextInputViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForumTextInputViewController.swift
+//  KomicaViewer
+//
+//  Created by Craig Zheng on 21/08/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+class ForumTextInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
