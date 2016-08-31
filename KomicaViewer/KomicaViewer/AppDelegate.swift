9e7291f2986ac17702b3b4e6044c5b092474988c
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-08-31 17:55:27 +1000

    Included firebase SDK, enabled admit and crash features.

--- KomicaViewer/KomicaViewer/AppDelegate.swift
+++ KomicaViewer/KomicaViewer/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import Firebase
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Use Firebase library to configure APIs
+        FIRApp.configure()
         Forums.updateRemoteForums()
         // Initialise Configuration singleton.
         _ = Configuration.singleton
