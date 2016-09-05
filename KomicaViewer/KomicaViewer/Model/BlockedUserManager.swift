2cde0e0e4ea66bb98ff7e884d293ec60dd854560
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-09-05 13:21:50 +1000

    Completed BlockedUserManager.

--- KomicaViewer/KomicaViewer/Model/BlockedUserManager.swift
+++ KomicaViewer/KomicaViewer/Model/BlockedUserManager.swift
@@ -9,5 +9,38 @@
 import UIKit
 
 class BlockedUserManager: NSObject {
-
+    static let sharedmanager = BlockedUserManager()
+    private let BlockedUserIDKey = "BlockedUserIDKey"
+    
+    private var blockedUserIDs = [String]()
+    
+    private func save() {
+        NSUserDefaults.standardUserDefaults().setObject(blockedUserIDs, forKey: BlockedUserIDKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    private func restore() {
+        if let restoredObjects = NSUserDefaults.standardUserDefaults().objectForKey(BlockedUserIDKey) as? [String] {
+            blockedUserIDs = restoredObjects
+        }
+    }
+    
+    override init() {
+        super.init()
+        restore()
+    }
+    
+    func blockUserID(id: String) {
+        blockedUserIDs.append(id)
+        save()
+    }
+    
+    func unblockUserID(id: String) {
+        blockedUserIDs.removeObject(id)
+        save()
+    }
+    
+    func isUserIDBlocked(id: String) -> Bool {
+        return blockedUserIDs.contains(id)
+    }
 }
