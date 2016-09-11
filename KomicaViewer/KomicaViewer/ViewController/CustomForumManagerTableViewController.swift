a29fbe0a6c17bef96ff2900ef035ea6b12567449
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-09-11 20:46:47 +1000

    Added CustomForumManagerTableView, which will be responsible for managing the custom boards.

---
+++ KomicaViewer/KomicaViewer/ViewController/CustomForumManagerTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomForumManagerTableViewController.swift
+//  KomicaViewer
+//
+//  Created by Craig Zheng on 11/09/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+class CustomForumManagerTableViewController: UITableViewController {
+
+}
