95734a2d8cc1b7cbffbcb6c57640f8d80a1f382f
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-11-02 21:53:27 +1100

    Added WebMPlayerViewController, which uses OGVPlayerKit to display a webM video.

---
+++ KomicaViewer/KomicaViewer/ViewController/WebMPlayerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WebMPlayerViewController.swift
+//  KomicaViewer
+//
+//  Created by Craig Zheng on 2/11/16.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+import OGVKit
+
+class WebMPlayerViewController: UIViewController {
+    
+    var webMSourceURL: NSURL!
+
+    @IBOutlet weak var playerView: OGVPlayerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        playerView.delegate = self
+        playerView.sourceURL = webMSourceURL
+        playerView.play()
+    }
+
+    @IBAction func doneAction(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func safariAction(sender: AnyObject) {
+        if webMSourceURL != nil {
+            UIApplication.sharedApplication().openURL(webMSourceURL)
+        }
+    }
+    
+}
+
+extension WebMPlayerViewController: OGVPlayerDelegate {
+    
+}
