0e8feaab863991d171a178353a2fee23f1eaf2ed
Haozheng Zheng <haozheng.zheng@optus.com.au>
2017-07-13 22:35:13 +1000

    Added test case for thread json encoding and decoding.

--- KomicaViewer/KomicaViewerTests/KomicaViewerTests.swift
+++ KomicaViewer/KomicaViewerTests/KomicaViewerTests.swift
@@ -13,18 +13,27 @@ import XCTest
 
 class KomicaViewerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let animeHtmlString = "<div class=\"reply\" id=\"r88457\"><input type=\"checkbox\" name=\"88457\" value=\"delete\" /><span class=\"title\">無念</span> Name <span class=\"name\"></span> [16/04/06(水)22:54 ID:FYhAFZQ2] <a href=\"pixmicat.php?res=88400&amp;page_num=all#r88457\" class=\"qlink\">No.88457</a> &nbsp;<br />&nbsp;画像ファイル名：<a href=\"http://2cat.twbbs.org/~tedc21thc/anime/src/1459954470573.jpg\" rel=\"_blank\">1459954470573.jpg</a>-(15 KB, 640x360) <small>[サムネ表示]</small><br /><a href=\"http://2cat.twbbs.org/~tedc21thc/anime/src/1459954470573.jpg\" rel=\"_blank\"><img src=\"http://2cat.twbbs.org/~tedc21thc/anime/thumb/1459954470573s.jpg\" style=\"width: 125px; height: 71px;\" class=\"img\" alt=\"15 KB\" title=\"15 KB\" /></a><div class=\"quote\">請問OP1裡的那女孩裙中，的&quot;那女孩&quot;到底是誰??</div></div>"
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testJsonEncoding() {
-        let thread = KomicaEngine.Thread()
-        
+    func testThreadJsonEncoding() {
+        if let animeElement = ObjectiveGumbo.parseDocument(with: animeHtmlString).elements(withClass: "reply").first as? OGElement {
+            let thread = PixmicatThreadParser.threadWithOGElement(animeElement)
+            let jsonString = thread?.jsonEncode()!
+            let decodedThread = KomicaEngine.Thread.jsonDecode(jsonDict: try! JSONSerialization.jsonObject(with: (jsonString?.data(using: .utf8))!, options: .allowFragments) as! Dictionary<String, AnyObject>)
+            XCTAssert(decodedThread != nil)
+            
+            if let decodedThread = decodedThread as? KomicaEngine.Thread {
+                XCTAssert(thread?.title == decodedThread.title)
+                XCTAssert(thread?.content?.string == decodedThread.content?.string)
+                XCTAssert(thread?.ID == decodedThread.ID)
+                XCTAssert(thread?.UID == decodedThread.UID)
+                XCTAssert(thread?.postDateString == decodedThread.postDateString)
+                XCTAssert(thread?.thumbnailURL == decodedThread.thumbnailURL)
+                XCTAssert(thread?.imageURL == decodedThread.imageURL)
+                XCTAssert(thread?.warnings.count == decodedThread.warnings.count)
+            } else {
+                XCTFail()
+            }
+        }
     }
 }
