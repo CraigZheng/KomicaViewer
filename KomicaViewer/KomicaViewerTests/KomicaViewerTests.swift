ed98ee0c9dbebe89ba55091d5932f6a75c5c45ff
Haozheng Zheng <haozheng.zheng@optus.com.au>
2017-07-13 22:52:12 +1000

    test jsonable for KomicaForum.

--- KomicaViewer/KomicaViewerTests/KomicaViewerTests.swift
+++ KomicaViewer/KomicaViewerTests/KomicaViewerTests.swift
@@ -15,6 +15,8 @@ class KomicaViewerTests: XCTestCase {
     
     let animeHtmlString = "<div class=\"reply\" id=\"r88457\"><input type=\"checkbox\" name=\"88457\" value=\"delete\" /><span class=\"title\">無念</span> Name <span class=\"name\"></span> [16/04/06(水)22:54 ID:FYhAFZQ2] <a href=\"pixmicat.php?res=88400&amp;page_num=all#r88457\" class=\"qlink\">No.88457</a> &nbsp;<br />&nbsp;画像ファイル名：<a href=\"http://2cat.twbbs.org/~tedc21thc/anime/src/1459954470573.jpg\" rel=\"_blank\">1459954470573.jpg</a>-(15 KB, 640x360) <small>[サムネ表示]</small><br /><a href=\"http://2cat.twbbs.org/~tedc21thc/anime/src/1459954470573.jpg\" rel=\"_blank\"><img src=\"http://2cat.twbbs.org/~tedc21thc/anime/thumb/1459954470573s.jpg\" style=\"width: 125px; height: 71px;\" class=\"img\" alt=\"15 KB\" title=\"15 KB\" /></a><div class=\"quote\">請問OP1裡的那女孩裙中，的&quot;那女孩&quot;到底是誰??</div></div>"
     
+    let forumString = "{\"name\":\"綜合學術討論\",\"header\":\"\",\"indexURL\":\"http://gzone-anime.info/UnitedSites/academic/\",\"listURL\":\"http://gzone-anime.info/UnitedSites/academic/<PAGE>.htm\",\"responseURL\":\"http://gzone-anime.info/UnitedSites/academic/pixmicat.php?res=<ID>\",\"postURL\":\"\",\"replyURL\":\"\",\"parserType\":0}"
+    
     func testThreadJsonEncoding() {
         if let animeElement = ObjectiveGumbo.parseDocument(with: animeHtmlString).elements(withClass: "reply").first as? OGElement {
             let thread = PixmicatThreadParser.threadWithOGElement(animeElement)
@@ -34,6 +36,21 @@ class KomicaViewerTests: XCTestCase {
             } else {
                 XCTFail()
             }
+        }
+    }
+    
+    func testForumJsonEncoding() {
+        let forum = KomicaForum.jsonDecode(jsonDict: try! JSONSerialization.jsonObject(with: forumString.data(using: .utf8)!, options: .allowFragments) as! Dictionary<String, AnyObject>)
+        let jsonString = (forum?.jsonEncode())!
+        let decodedForum = KomicaForum.jsonDecode(jsonDict: try! JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!, options: .allowFragments) as! Dictionary<String, AnyObject>)
+        
+        XCTAssert(forum != nil && decodedForum != nil)
+        
+        if let forum = forum as? KomicaForum,
+            let decodedForum = decodedForum as? KomicaForum {
+            XCTAssert(decodedForum == forum)
+        } else {
+            XCTFail()
         }
     }
 }
